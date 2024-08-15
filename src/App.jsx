1dda2aaa9f8d8c329e351c1c3b87d1e164854100
zmguitarrad <zguitarrad@gmail.com>
2024-08-14 20:43:08 -0500

    add features in card and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
-import Card from "./Card";
-
+import React, { useState } from 'react';
+import Card from './Card';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [color, setColor] = useState('#000000');
+  const [error, setError] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleColorChange = (e) => {
+    setColor(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError('');
+    setSubmitted(false);
+
+    // Validaciones
+    if (title.trim().length < 3 || title.startsWith(' ')) {
+      setError('Por favor chequea que la información sea correcta');
+      return;
+    }
+    setSubmitted(true);
+  };
+
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      <h1>Elige un color</h1>
+      <form onSubmit={handleSubmit} style={{ border: `2px solid, white`, padding: '10px', borderRadius: '5px', marginTop: '20px' }}>
+        <div >
+          <label>Nombre:</label> <br />
+          <input 
+            type="text" 
+            value={title} 
+            onChange={handleTitleChange} 
+            required
+            placeholder='Ingresa tu nombre' 
+          />
+        </div>
+        <div>
+          <label>Color favorito:</label> <br />
+          <input 
+            type="text" 
+            value={color} 
+            onChange={handleColorChange}
+            placeholder='Ingresa tu color favorito' 
+          />
+        </div>
+        <button style={{padding: '10px', borderRadius: '5px', marginTop: '20px', backgroundColor: 'blue' }} type="submit">Enviar</button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {submitted && <Card title={title} color={color} />}
     </div>
   );
 }
-
 
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
 
-
-
-
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
-};
-
+function Card({ title,  color }) {
+  return (
+    <div  style={{ border: `2px solid, white`, padding: '10px', borderRadius: '5px', marginTop: '20px' }} >
+      <h2>Hola {title} !</h2>
+      <h2>Sabemos que tu color favorito es: </h2>
+      <h2 style={{ backgroundColor: ` ${color}`, padding: '10px', borderRadius: '5px', marginTop: '20px', }}> {color}</h2>
+    </div>
+  );
+}
 
 export default Card;
